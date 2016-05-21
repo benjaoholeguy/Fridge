75e7ea7c5920c1d1ed831551d4f2ddf96ef67e89
Benjamin Oholeguy <benjaoholeguy@gmail.com>
2016-05-21 13:10:26 +1200

    log in page added

---
+++ www/js/index1.js
@@ -0,0 +1,43 @@
+/**
+ * Created by benja on 21/05/2016.
+ */
+var app;
+app = {
+    // Application Constructor
+    initialize: function () {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // `load`, `deviceready`, `offline`, and `online`.
+    bindEvents: function () {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+        //document.getElementById('scan').addEventListener('click', this.scan, false);
+        //document.getElementById('encode').addEventListener('click', this.encode, false);
+    },
+
+    // deviceready Event Handler
+    //
+    // The scope of `this` is the event. In order to call the `receivedEvent`
+    // function, we must explicity call `app.receivedEvent(...);`
+    //
+    onDeviceReady: function () {
+        app.receivedEvent('deviceready');
+        
+
+
+    },
+
+
+    // Update DOM on a Received Event
+    receivedEvent: function (id) {
+        
+    }
+
+
+};
+
+function validate(){
+    location.href="index2.html";
+}
