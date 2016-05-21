80269f816f9547c7173726fc1fed4573578c21d9
Benjamin Oholeguy <benjaoholeguy@gmail.com>
2016-05-21 18:21:38 +1200

    images added

--- www/js/index.js
+++ www/js/index.js
@@ -81,10 +81,10 @@ function share(expr){
 function list(){
 
     var ret='<div class="col-lg-12"><h1 class="page-header">Food</h1></div>';
-    for (var i=0; i<10; i++){
+    for (var i=1; i<11; i++){
         ret+='<div class="col-lg-3 col-md-4 col-xs-6 thumb">' +
             '<a class="thumbnail" href="#">' +
-            '<img class="img-responsive" src="http://placehold.it/400x300" alt="">' +
+            '<img class="img-responsive" src="img/food/2.jpg" alt="">' +
             '</a>' +
             '</div>';
     }
